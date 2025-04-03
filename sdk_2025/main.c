1933222d7a765752bc2625f1fcebd177c1cf55df
jlb <JLB-P@users.noreply.github.com>
2025-04-03 16:16:30 -0600

    upd030425

--- sdk_2025/main.c
+++ sdk_2025/main.c
@@ -10,14 +10,14 @@ Memory and bus architecture (p.46)
 
 int main(void){
 	init_gpio();
-	led_on_off(50);
+	//led_on_off(50);
 	//led_onoff_timer2(1000);
 	
 	
 	/*Using timers
 	***************/
-	//timer2_output_compare();
-	//timer2_pwm();
+	timer2_ch1_oc();
+	timer3_ch1_pwm();
 	while(1){
 		
 	}
